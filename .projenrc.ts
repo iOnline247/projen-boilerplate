39d20dfed8b563928bd84a95c3dbc0986c4c9ca4
Matthew Bramer <mbramer@pansophiclearning.com>
2023-01-23 11:01:11 -0500

    Convert to jsii project

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,15 +1,38 @@
-import { ProjenProjectFromGit } from '@mrgrain/projen-from-git';
+import { javascript, release } from "projen";
+import { ProjenProjectFromGit } from "@mrgrain/projen-from-git";
 
 const project = new ProjenProjectFromGit({
-  author: 'Matthew Bramer',
-  authorAddress: 'mbramer@pansophiclearning.com',
-  defaultReleaseBranch: 'main',
-  devDeps: ['@mrgrain/projen-from-git@mrgrain/projen-from-git'],
-  name: 'boilerplate',
-  repositoryUrl: 'https://git-codecommit.us-east-1.amazonaws.com/v1/repos/boilerplate',
+  author: "Matthew Bramer",
+  authorAddress: "mbramer@pansophiclearning.com",
+  name: "boilerplate",
+  repositoryUrl:
+    "https://git-codecommit.us-east-1.amazonaws.com/v1/repos/boilerplate",
+  defaultReleaseBranch: "deploy",
+  projenrcTs: true,
+  github: false,
+  githubOptions: {
+    mergify: false,
+  },
+  packageManager: javascript.NodePackageManager.NPM,
+  releaseTrigger: release.ReleaseTrigger.manual(),
+  releaseToNpm: false,
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      printWidth: 120,
+      singleQuote: true,
+      trailingComma: javascript.TrailingComma.ALL,
+    },
+  },
+  deps: ["aws-cdk-lib@2.40.0", "constructs@10.1.94"],
+  devDeps: [
+    "@mrgrain/projen-from-git@mrgrain/projen-from-git",
+    "typescript",
+    "projen",
+  ],
 
-  // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
 project.synth();
