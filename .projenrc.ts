c025d778b9b27429cd3b40d4a56afa8b00fa6b58
Matthew Bramer <515152+iOnline247@users.noreply.github.com>
2023-01-31 10:14:05 -0500

    Update .projenrc.ts

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,7 +4,7 @@ import { javascript, release } from 'projen';
 const project = new ProjenProjectFromGit({
   author: 'Matthew Bramer',
   name: 'boilerplate',
-  repositoryUrl: 'https://git-codecommit.us-east-1.amazonaws.com/v1/repos/boilerplate',
+  repositoryUrl: 'https://github.com/iOnline247/projen-boilerplate',
   defaultReleaseBranch: 'deploy',
   projenrcTs: true,
   github: false,
